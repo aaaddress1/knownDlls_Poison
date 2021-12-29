6ee837b062ba00985964d830d5095cba7541fbf5
Sheng-Hao Ma <aaaddress1@gmail.com>
2021-12-29 12:59:38 +0800

    Update knownDlls_Poison.cpp

--- source/knownDlls_Poison/knownDlls_Poison.cpp
+++ source/knownDlls_Poison/knownDlls_Poison.cpp
@@ -1,11 +1,7 @@
-﻿// knownDlls_Poison.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
-//
-
 #include <iostream>
 #include <windows.h>
 #include <AclAPI.h>
 #include "ImpersonateToken/hijackToken.h"
-
 
 _Success_(return)
 BOOL FindFileForTransaction(_In_ DWORD dwMinSize, _Out_ LPWSTR * ppwszFilePath)
